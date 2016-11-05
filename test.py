7e097a22720d36c0a5826a6b7263757ac5450d6a
abhishekkodi <aknp3@mst.edu>
2016-11-05 09:25:49 -0400

    Create test.py
    Template flow chart to use

---
+++ test.py
@@ -0,0 +1,34 @@
+# Welcome message
+print("Welcome to AK Machine Learning fair")
+
+# Qeastions to takle and theoretical understading
+
+
+# Breaking the question down to machine learning understanding and smaller bits to beat
+
+
+# Way to go to make it work and hypothesis
+
+-------------------------
+
+# Import
+
+
+# Data input test
+
+
+# Data input for run
+
+
+# Algorithm and classifier defining
+
+
+# Running the multiple algorithms on test data and score values
+
+
+# Predicting for the real data.
+
+
+# Feedback and choosing the best algorithm with reasons.
+
+
